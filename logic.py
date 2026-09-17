3341da9b638c4a36d06b6d245ef6756522e90e8e
Blackbottt <ryanchipir@gmail.com>
2026-09-17 22:01:54 +0200

    removed all functions in logic

--- logic.py
+++ logic.py
@@ -1,22 +1,3 @@
 from database import create_connection
 
-def add_job_application(company, position, status, date_applied):
-    db_command = """INSERT INTO job_applications (company, position, status, date_applied)
-    VALUES (?, ?, ?, ?)""", (company, position, status, date_applied)
-    job_application_command(db_command)
-            
-
-def job_application_command(command):
-    conn = create_connection("job_applications.db")
-    if conn is not None:
-        try:
-            cursor = conn.cursor()
-            cursor.execute(command)
-            results = cursor.fetchall()
-            return results
-        except Exception as e:
-            print(f"An error occurred while executing the command: {e}")
-            return None
-        finally:
-            conn.close()
 
