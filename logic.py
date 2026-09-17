40789a71d39b33f1627ffb42748c68149bf36282
Blackbottt <ryanchipir@gmail.com>
2026-09-17 21:38:50 +0200

    changed add job application function to keep only the sql command

--- logic.py
+++ logic.py
@@ -1,22 +1,10 @@
 from database import create_connection
 
 def add_job_application(company, position, status, date_applied):
-    conn = create_connection("job_applications.db")
-    if conn is not None:
-        try:
-            cursor = conn.cursor()
-            cursor.execute("""
-                INSERT INTO job_applications (company, position, status, date_applied)
-                VALUES (?, ?, ?, ?)
-            """, (company, position, status, date_applied))
-            conn.commit()
-            print("Job application added successfully.")
-        except Exception as e:
-            print(f"An error occurred while adding the job application: {e}")
-        finally:
-            conn.close()
-    else:
-        print("Error! Cannot create the database connection.")
+    db_command = """INSERT INTO job_applications (company, position, status, date_applied)
+    VALUES (?, ?, ?, ?)""", (company, position, status, date_applied)
+    job_application_command(db_command)
+            
 
 def job_application_command(command):
     conn = create_connection("job_applications.db")
@@ -31,3 +19,4 @@ def job_application_command(command):
             return None
         finally:
             conn.close()
+
