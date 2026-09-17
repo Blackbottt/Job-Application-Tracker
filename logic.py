c7dd473f51aa037b918750725bd1524b1dd19100
Blackbottt <ryanchipir@gmail.com>
2026-09-17 22:42:48 +0200

    added in logic.py add job application function

--- logic.py
+++ logic.py
@@ -1,16 +1,19 @@
 from database import create_connection
 
-def add_job_application():
+def add_job_application(company, position, status, date_applied):
     conn = create_connection("job_applications.db")
-
     if conn is not None:
         try:
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO applications (company, position, status, applied_date, job_url, notes)"
-VALUES (?, ?, ?, ?, ?, ?))
+            cursor.execute("""
+                INSERT INTO job_applications (company, position, status, date_applied)
+                VALUES (?, ?, ?, ?)
+            """, (company, position, status, date_applied))
             conn.commit()
-
-        except Sqlite3.Error as e:
-            print(f"Datanase error: {e}")
-
-        conn.close()
+            print("Job application added successfully.")
+        except Exception as e:
+            print(f"An error occurred while adding the job application: {e}")
+        finally:
+            conn.close()
+    else:
+        print("Error! Cannot create the database connection.")
