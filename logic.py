b5cf4808fbaa780e57d5f79e1865b43394b4ba6c
Blackbottt <ryanchipir@gmail.com>
2026-09-17 22:36:45 +0200

    added logic.py

--- logic.py
+++ logic.py
@@ -1,3 +1,16 @@
 from database import create_connection
 
+def add_job_application():
+    conn = create_connection("job_applications.db")
 
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO applications (company, position, status, applied_date, job_url, notes)"
+VALUES (?, ?, ?, ?, ?, ?))
+            conn.commit()
+
+        except Sqlite3.Error as e:
+            print(f"Datanase error: {e}")
+
+        conn.close()
