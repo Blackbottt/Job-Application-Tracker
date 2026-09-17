58789eb2e2797846185d925cee4d8b66201ed8c3
Blackbottt <ryanchipir@gmail.com>
2026-09-17 23:48:15 +0200

    added const variables to Js

--- static/app.js
+++ static/app.js
@@ -1,2 +1,7 @@
 const table = document.getElementById('applications-table');
-
+const applicationsForm = document.getElementById('applications-form');
+const applicationId = document.getElementById('application-id');
+const company = document.getElementById('company');
+const position = document.getElementById('position');
+const applicationDate = document.getElementById('application-date');
+const applicationsForm = document.getElementById('applications-form');
