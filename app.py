4f1b1ce7f1cdfd3af7720f405adadb58a9446099
Blackbottt <ryanchipir@gmail.com>
2026-09-18 08:33:26 +0200

    added add job applications route

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ def home():
 @app.route('/applications')
 def get_job_applications():
     applications = logic.get_job_applications()
+    print(applications)
     return jsonify(applications)
+
+@app.route('/applications', method=['POST'])
+def add_job_applications():
+    return logic.add_job_application()
 
 if __name__ == '__main__':
     app.run(debug=True)
