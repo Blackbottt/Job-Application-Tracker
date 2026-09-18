ba47c16ac4a34dcbffbfb356a3f6bae8b8c1007b
Blackbottt <ryanchipir@gmail.com>
2026-09-18 16:25:46 +0200

    moved from created table element to it appending tableHeader out of loadApplications function

--- static/app.js
+++ static/app.js
@@ -1,4 +1,3 @@
-const table = document.getElementById('applications-table');
 const applicationsForm = document.getElementById('applications-form');
 const company = document.getElementById('company');
 const position = document.getElementById('position');
@@ -6,8 +5,37 @@ const applicationDate = document.getElementById('application-date');
 const statusOfApplication = document.getElementById('status');
 const jobUrl = document.getElementById('job-url');
 const notes = document.getElementById('notes');
+const dashboard = document.getElementById('dashboard');
 
-const dashboard = document.getElementById('dashboard')
+const table = document.createElement('table');
+const caption = document.createElement('caption');
+const tableHeader = document.createElement('tr');
+
+table.classList.add('applications-table');
+caption.classList.add('applications-caption');
+tableHeader.classList.add('table-row');
+caption.textContent = 'Job Applications';
+
+const headers = [
+    'ID',
+    'Company',
+    'Position',
+    'Application Date',
+    'Status',
+    'Notes',
+    'Job URL',
+    'Created At'
+];
+
+headers.forEach(header => {
+    const tableHeading = document.createElement('th');
+    tableHeading.textContent = header;
+    tableHeading.classList.add('table-header');
+    tableHeader.appendChild(tableHeading);
+});
+
+table.appendChild(caption);
+table.appendChild(tableHeader);
 
 async function addApplication() {
     await fetch('/applications', {
@@ -29,35 +57,6 @@ async function addApplication() {
 async function loadApplications() {
     const response = await fetch('/applications');
     const applications = await response.json();
-    const table = document.createElement('table');
-    const caption = document.createElement('caption');
-    const tableHeader = document.createElement('tr');
-
-    table.classList.add('applications-table');
-    caption.classList.add('applications-caption');
-    tableHeader.classList.add('table-row');
-    caption.textContent = 'Job Applications';
-
-    const headers = [
-        'ID',
-        'Company',
-        'Position',
-        'Application Date',
-        'Status',
-        'Notes',
-        'Job URL',
-        'Created At'
-    ];
-
-    headers.forEach(header => {
-        const tableHeading = document.createElement('th');
-        tableHeading.textContent = header;
-        tableHeading.classList.add('table-header');
-        tableHeader.appendChild(tableHeading);
-    })
-
-    table.appendChild(caption);
-    table.appendChild(tableHeader);
 
     applications.forEach(application => {
         const tableRow = document.createElement('tr');
@@ -79,7 +78,7 @@ async function loadApplications() {
         table.appendChild(tableRow);
     });
     dashboard.appendChild(table);
-    console.log("App: ", applications)
+    console.log("App: ", applications);
 }
 
 applicationsForm.addEventListener("submit", e => {
