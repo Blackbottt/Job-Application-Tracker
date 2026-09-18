d746314177e939ab39fa0753dbdc9058649d67d3
Blackbottt <ryanchipir@gmail.com>
2026-09-18 21:07:14 +0200

    added edit job applications route with return

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def edit_job_application(id):
     job_posting_url = data.get('job_posting_url')
     notes = data.get('notes')
     logic.edit_job_application(company_name, position, status, date_applied, notes, job_posting_url, id)
+    return {"message": "Job application added successfully."}, 201
 
 @app.route('/applications/<int:id>', methods=["DELETE"])
 def delete_job_application(id):
