24be6ca7eb4fee53cd97c56a815ba6575ca8ad6b
Blackbottt <ryanchipir@gmail.com>
2026-09-19 07:52:28 +0200

    logic.py had all cursor.rowcounts fixed to the attribute it is

--- logic.py
+++ logic.py
@@ -13,7 +13,7 @@ def add_job_application(company_name, position, status, date_applied, notes, job
             """, (company_name, position, status, date_applied, notes, job_posting_url))
             conn.commit()
             print("Job application added successfully.")
-            return cursor.rowcount()
+            return cursor.rowcount
         except Exception as e:
             print(f"An error occurred while adding the job application: {e}")
         finally:
@@ -65,7 +65,7 @@ def edit_job_application(company_name, position, status, date_applied, notes, jo
             )
             conn.commit()
             print(f"Record with ID {id} updated successfully.")
-            return cursor.rowcount()
+            return cursor.rowcount
 
         except Exception as e:
             print(f"An error occurred while adding the job application: {e}")
@@ -81,7 +81,7 @@ def delete_job_application(id):
             cursor = conn.cursor()
             cursor.execute("""DELETE FROM job_applications WHERE id = ?""", (id,))
             conn.commit()
-            return cursor.rowcount()
+            return cursor.rowcount
         except Exception as e:
             print(f"An error occurred while adding the job application: {e}")
         finally:
@@ -96,7 +96,7 @@ def delete_job_applications():
             cursor = conn.cursor()
             cursor.execute("""DELETE FROM job_applications""")
             conn.commit()
-            return cursor.rowcount()
+            return cursor.rowcount
         except Exception as e:
             print(f"An error occurred while adding the job application: {e}")
         finally:
